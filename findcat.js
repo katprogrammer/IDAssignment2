ad889372dee17a8af4319ad7091e1e16379dfec1
WaiKeat <wklai368@gmail.com>
2021-01-08 15:05:07 +0800

    Add style and javascript for search map function

--- findcat.js
+++ findcat.js
@@ -5,6 +5,36 @@ function createMap() {
         zoom: 10,
     };
     map = new google.maps.Map(document.getElementById('map'), options);
+    var input = document.getElementById('search');
+    var searchBox = new google.maps.places.SearchBox(input);
+    map.addListener('bounds_changed', function() {
+        searchBox.setBounds(map.getBounds());
+    });
+    var markers = [];
+    searchBox.addListener('places_changed', function () {
+        var places = searchBox.getPlaces();
+        if (places.lenth === 0)
+        return;
+        markers.forEach(function (m) {m.setMap(null) });
+        markers = [];
+        var bounds = new google.maps.LatLngBounds();
+        places.forEach(function(p) {
+            if (!p.geometry)
+                return;
+
+            markers.push(new google.maps.Marker ({
+                map: map,
+                title: p.name,
+                position: p.geometry.location
+            }));
+
+            if (p.geometry.viewport)
+                bounds.union(p.geometry.viewport);
+            else
+                bounds.extend(p.geometry.location);
+        });
+        maps.fitBounds(bounds);
+    });
     infowindow = new google.maps.InfoWindow;
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function (p) {
@@ -23,6 +53,7 @@ function createMap() {
         handleLocationError('No geolocation available', map.center());
 
     }
+
 }
 
 function handleLocationError(content, position) {
